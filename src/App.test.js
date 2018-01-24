37042c8daaaa4c7744e971cc29126c90f1beec91
Marko Harjula <marko.harjula@gmail.com>
2018-01-24 07:25:10 +0200

    fixed testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
+
+jest.mock('react-i18next', () => ({
+	translate: () => Component => props => <Component t={() => ''} {...props} />,
+}));
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+	const div = document.createElement('div');
+	const elem = ReactDOM.render(<App />, div);
+	ReactDOM.unmountComponentAtNode(div);
+});
+
+test('component rendering', () => {
+	const component = renderer.create(
+		<App />,
+	);
+	let tree = component.toJSON();
 });
