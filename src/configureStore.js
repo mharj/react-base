6e742ea039ed4961a69f54a7457017b3928e1eb9
Marko Harjula <marko.harjula@gmail.com>
2018-06-08 10:02:22 +0300

    npm up + redux debug option

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,5 +1,5 @@
 
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {persistStore, persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
@@ -10,16 +10,32 @@ const persistConfig = {
 	storage: storage,
 };
 
+const initialState = {
+	app: {
+		error: null,
+		etag: null,
+	}
+};
+
+const enhancers = [];
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+if (process.env.NODE_ENV === 'development') {
+	const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+	if (typeof devToolsExtension === 'function') {
+		enhancers.push(devToolsExtension());
+	}
+}
+const composedEnhancers = compose(
+	applyMiddleware(thunk),
+	...enhancers,
+);
 
 export default () => {
 	let store = createStore(
 		persistedReducer,
-		{app: {
-			error: null,
-			etag: null,
-		}},
-		applyMiddleware(thunk)
+		initialState,
+		composedEnhancers,
 	);
 	let persistor = persistStore(store);
 	return {store, persistor};
