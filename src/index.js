6696a2da44ce4323f9846881ec6a454202ecabaa
Marko Harjula <marko.harjula@gmail.com>
2018-08-02 15:26:13 +0300

    more code split

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
 import {I18nextProvider} from 'react-i18next';
-import i18n from './i18n';
 import './index.css';
 import App from './App';
 import {PersistGate} from 'redux-persist/lib/integration/react';
 import {Provider} from 'react-redux';
 import ServiceWorkerProvider from './ServiceWorkerProvider';
-import configureStore from './configureStore';
 import('babel-polyfill');
 import('whatwg-fetch');
-let {store, persistor} = configureStore();
+Promise.all([
+	import('./configureStore'),
+	import('./i18n'),
+])
+.then( (loaded) => {
+	const [configureStore, i18n] = loaded;
+	let {store, persistor} = configureStore.default();
+	ReactDOM.render(
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<I18nextProvider i18n={i18n.default}>
+					<ServiceWorkerProvider>
+						<App />
+					</ServiceWorkerProvider>
+				</I18nextProvider>
+			</PersistGate>
+		</Provider>,
+		document.getElementById('root'),
+	);
+})
 
-ReactDOM.render(
-	<Provider store={store}>
-		<PersistGate loading={null} persistor={persistor}>
-			<I18nextProvider i18n={i18n}>
-				<ServiceWorkerProvider>
-					<App />
-				</ServiceWorkerProvider>
-			</I18nextProvider>
-		</PersistGate>
-	</Provider>,
-	document.getElementById('root'),
-);
