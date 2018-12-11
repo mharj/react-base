9f1cb805f248d0502196261f16e84fda50b662d0
Marko Harjula <marko.harjula@gmail.com>
2018-12-11 10:36:49 +0200

    ver up + Blob download demo in Secret View

--- src/views/Secret.js
+++ src/views/Secret.js
@@ -3,6 +3,11 @@ import {Helmet} from 'react-helmet';
 import {withNamespaces} from 'react-i18next';
 
 class Secret extends React.Component {
+	constructor(props) {
+		super(props);
+		const debug = {hello: 'world'};
+		this.dataBlob = new Blob([JSON.stringify(debug, null, 2)], {type: 'application/json'});
+	}
 	render() {
 		return (
 			<div>
@@ -10,6 +15,7 @@ class Secret extends React.Component {
 					<title>Secret</title>
 				</Helmet>
 				<div>some secret stuff</div>
+				<a href={URL.createObjectURL(this.dataBlob)} download={'debug.json'}>Download</a>
 			</div>
 		);
 	}
